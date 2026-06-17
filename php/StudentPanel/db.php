7506aea01b4dacf1205fbd4ad727b0d508bc0a72
Sanika Gawade <sanikagawade010@gmail.com>
2026-06-17 12:34:44 +0530

    Updated homepage design

--- php/StudentPanel/db.php
+++ php/StudentPanel/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("db","root","root","sports_ms");
+$conn = new mysqli("db","root","root","sports_db");
 
 if($conn->connect_error){
   die("Connection failed: " . $conn->connect_error);
